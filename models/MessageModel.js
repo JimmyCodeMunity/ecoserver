29b5bee262626296db69db8e1615ccb8df0270e1
JimmyCodeMunity <jameswafula2002@gmail.com>
2024-12-07 20:28:40 +0300

    credentials deleted

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -23,5 +23,5 @@ const messageSchema = new mongoose.Schema({
   },
 });
 
-const Message = mongoose.model("Message", messageSchema);
+const Message = mongoose.model("message", messageSchema);
 module.exports = Message;
